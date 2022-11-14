140d1cc7fa88deda0a3252188e70f4f81d1c1603
k021g1131@g.neec.ac.jp <mk021g1131@g.neec.ac.jp>
2022-11-14 14:52:15 +0900

    点からジオメトリ生成

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,29 +1,37 @@
 #include"BasicShaderHeader.hlsli"
 
-[maxvertexcount(6)]
+[maxvertexcount(3)]
 void main(
-	triangle VSOutput input[3] : SV_POSITION,
+	point VSOutput input[1] : SV_POSITION,
 	inout TriangleStream< GSOutput > output
 )
 {
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput elemdent;
-		elemdent.svpos = input[i].svpos;
-		elemdent.normal = input[i].normal;
-		elemdent.uv = input[i].uv;
-		output.Append(elemdent);
-	}
+	GSOutput elemdent;
+	//共通
+	elemdent.normal = input[0].normal;
+	elemdent.uv = input[0].uv;
+	//1点目
+	elemdent.svpos = input[0].svpos;
+	output.Append(elemdent);
+	//2点目
+	elemdent.svpos = input[0].svpos+float4(10.0f,10.0f,0,0);
+	output.Append(elemdent);
+	//3点目
+	elemdent.svpos = input[0].svpos + float4(10.0f, 0, 0, 0);
+	output.Append(elemdent);
+	//for (uint i = 0; i < 3; i++)
+	//{
+	//}
 	//現在のストリップを終了
-	output.RestartStrip();
+	//output.RestartStrip();
 
-	for (uint i = 0; i < 3; i++)
+	/*for (uint i = 0; i < 3; i++)
 	{
 		GSOutput elemdent;
 		elemdent.svpos = input[i].svpos+float4(20.0f,0.0f,0.0f,0.0f);
 		elemdent.normal = input[i].normal;
 		elemdent.uv = input[i].uv * 5.0f;
 		output.Append(elemdent);
-	}
+	}*/
 
 }
