52d688c3b527692beea5319d6247c86b54a40715
k021g1131@g.neec.ac.jp <mk021g1131@g.neec.ac.jp>
2022-11-28 14:24:08 +0900

    完成

---
+++ Resources/GameScene.cpp
@@ -0,0 +1,236 @@
+﻿#include "GameScene.h"
+#include <cassert>
+
+using namespace DirectX;
+
+GameScene::GameScene()
+{
+}
+
+GameScene::~GameScene()
+{
+	delete spriteBG;
+	delete particleMan;
+	delete object3d;
+	delete	nbObj;
+	
+	safe_delete(sprite1);
+	safe_delete(sprite2);
+}
+
+void GameScene::Initialize(DirectXCommon* dxCommon, Input* input)
+{
+	// nullptrチェック
+	assert(dxCommon);
+	assert(input);
+
+	this->dxCommon = dxCommon;
+	this->input = input;
+
+	// デバッグテキスト用テクスチャ読み込み
+	Sprite::LoadTexture(debugTextTexNumber, L"Resources/debugfont.png");
+	// デバッグテキスト初期化
+	debugText.Initialize(debugTextTexNumber);
+
+	// テクスチャ読み込み
+	Sprite::LoadTexture(1, L"Resources/background.png");
+
+	// 背景スプライト生成
+	spriteBG = Sprite::Create(1, { 0.0f,0.0f });
+	// 3Dオブジェクト生成
+	particleMan = ParticleManager::Create();
+	particleMan->Update();
+
+	object3d = Object3d::Create();
+	object3d->Update();
+
+	nbObj = NoBillObj::Create();
+	nbObj->Update();
+
+
+	Sprite::LoadTexture(2, L"Resources/texture.png");
+	sprite1 = Sprite::Create(2, { 0,0 });
+	sprite2 = Sprite::Create(2, { 500,500 }, { 1,0,0,1 }, { 0,0 }, false, true);
+
+	scene = Scene::one;
+}
+
+void GameScene::Update()
+{
+	if (scene==Scene::one)
+	{
+		// オブジェクト移動
+		if (input->PushKey(DIK_UP) || input->PushKey(DIK_DOWN) || input->PushKey(DIK_RIGHT) || input->PushKey(DIK_LEFT))
+		{
+			// 現在の座標を取得
+			XMFLOAT3 position = object3d->GetPosition();
+			XMFLOAT3 position2 = nbObj->GetPosition();
+
+
+			// 移動後の座標を計算
+			if (input->PushKey(DIK_UP)) { position.y += 1.0f; }
+			else if (input->PushKey(DIK_DOWN)) { position.y -= 1.0f; }
+			if (input->PushKey(DIK_RIGHT)) { position.x += 1.0f; }
+			else if (input->PushKey(DIK_LEFT)) { position.x -= 1.0f; }
+
+			if (input->PushKey(DIK_UP)) { position2.y += 1.0f; }
+			else if (input->PushKey(DIK_DOWN)) { position2.y -= 1.0f; }
+			if (input->PushKey(DIK_RIGHT)) { position2.x += 1.0f; }
+			else if (input->PushKey(DIK_LEFT)) { position2.x -= 1.0f; }
+
+			// 座標の変更を反映
+			object3d->SetPosition(position);
+			nbObj->SetPosition(position2);
+
+		}
+	}
+	else if (scene == Scene::two)
+	{
+		//パーティクル
+		for (int i = 0; i < 100; i++)
+		{
+			//XYZ全て[-5.0f,+5.0f]でランダムに分布
+			const	float	rnd_pos = 10.0f;
+			XMFLOAT3	pos{};
+			pos.x = (float)rand() / RAND_MAX * rnd_pos - rnd_pos / 2.0f;
+			pos.y = (float)rand() / RAND_MAX * rnd_pos - rnd_pos / 2.0f;
+			pos.z = (float)rand() / RAND_MAX * rnd_pos - rnd_pos / 2.0f;
+
+			//XYZ全て[-0.05f,+0.05f]でランダムに分布
+			const	float	rnd_vel = 0.1f;
+			XMFLOAT3	vel{};
+			vel.x = (float)rand() / RAND_MAX * rnd_vel - rnd_vel / 2.0f;
+			vel.y = (float)rand() / RAND_MAX * rnd_vel - rnd_vel / 2.0f;
+			vel.z = (float)rand() / RAND_MAX * rnd_vel - rnd_vel / 2.0f;
+
+			//重力に見立ててYのみ[-0.001f,0]でランダムに分布
+			const	float	rnd_acc = 0.001f;
+			XMFLOAT3	acc{};
+			acc.y = (float)rand() / RAND_MAX * rnd_acc;
+
+			particleMan->Add(60, pos, vel, acc);
+		}
+	}
+
+	// カメラ移動
+	if (input->PushKey(DIK_W) || input->PushKey(DIK_S) || input->PushKey(DIK_D) || input->PushKey(DIK_A))
+	{
+		if (scene == Scene::one)
+		{
+			if (input->PushKey(DIK_W)) { Object3d::CameraMoveEyeVector({ 0.0f,+1.0f,0.0f }); }
+			else if (input->PushKey(DIK_S)) { Object3d::CameraMoveEyeVector({ 0.0f,-1.0f,0.0f }); }
+			if (input->PushKey(DIK_D)) { Object3d::CameraMoveEyeVector({ +1.0f,0.0f,0.0f }); }
+			else if (input->PushKey(DIK_A)) { Object3d::CameraMoveEyeVector({ -1.0f,0.0f,0.0f }); }
+			if(input->PushKey(DIK_W)) { NoBillObj::CameraMoveEyeVector({ 0.0f,+1.0f,0.0f }); }
+			else if (input->PushKey(DIK_S)) { NoBillObj::CameraMoveEyeVector({ 0.0f,-1.0f,0.0f }); }
+			if (input->PushKey(DIK_D)) { NoBillObj::CameraMoveEyeVector({ +1.0f,0.0f,0.0f }); }
+			else if (input->PushKey(DIK_A)) { NoBillObj::CameraMoveEyeVector({ -1.0f,0.0f,0.0f }); }
+		}
+		else if (scene == Scene::two)
+		{
+			if (input->PushKey(DIK_W)) { ParticleManager::CameraMoveEyeVector({ 0.0f,+1.0f,0.0f }); }
+			else if (input->PushKey(DIK_S)) { ParticleManager::CameraMoveEyeVector({ 0.0f,-1.0f,0.0f }); }
+			if (input->PushKey(DIK_D)) { ParticleManager::CameraMoveEyeVector({ +1.0f,0.0f,0.0f }); }
+			else if (input->PushKey(DIK_A)) { ParticleManager::CameraMoveEyeVector({ -1.0f,0.0f,0.0f }); }
+		}
+	}
+
+	if (input->PushKey(DIK_SPACE))
+	{
+		//現在の座標を取得
+		XMFLOAT2	position = sprite1->GetPosition();
+		//移動後の座標を取得
+		position.x += 1.0f;
+		//座標の変更を反映
+		sprite1->SetPosition(position);
+	}
+
+	if (input->TriggerKey(DIK_F))
+	{ 
+		scene++;
+		if (scene==Scene::max)
+		{
+			scene = Scene::one;
+		}
+	}
+	if (scene == Scene::one)
+	{
+		
+		object3d->Update();
+		nbObj->Update();
+	}
+	else if (scene == Scene::two)
+	{
+		particleMan->Update();
+	}
+}
+
+void GameScene::Draw()
+{
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* cmdList = dxCommon->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(cmdList);
+	// 背景スプライト描画
+	spriteBG->Draw();
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	{
+		Object3d::PreDraw(cmdList);
+		if (scene == Scene::one)
+		{
+			object3d->Draw();
+		}
+		Object3d::PostDraw();
+		NoBillObj::PreDraw(cmdList);
+		if (scene == Scene::one)
+		{
+			nbObj->Draw();
+		}
+		NoBillObj::PostDraw();
+	}
+	{
+		// 3Dオブジェクト描画前処理
+		ParticleManager::PreDraw(cmdList);
+		if (scene == Scene::two)
+		{
+			// 3Dオブクジェクトの描画
+			particleMan->Draw();
+		}
+		/// <summary>
+		/// ここに3Dオブジェクトの描画処理を追加できる
+		/// </summary>
+
+		// 3Dオブジェクト描画後処理
+		ParticleManager::PostDraw();
+	}
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(cmdList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+	//sprite1->Draw();
+	//sprite2->Draw();
+	// デバッグテキストの描画
+	debugText.DrawAll(cmdList);
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+#pragma endregion
+}
